a2d6d45c07256124d04d34be8cde536bd0ed0b17
mhamza-ali <hamza4841333@gmail.com>
2023-09-20 17:33:06 +0500

    variables learned

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,1 +1,11 @@
 console.log("Hello world in js")
+
+//variables
+const accountId = 12 // change of value is not allowed
+let accountEmail = "hello@gmail.com"
+var accountPassword = "12345"
+accountCity = "Karachi"
+
+console.log(accountId)
+console.table([accountId, accountEmail, accountPassword])
+
