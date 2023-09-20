cd8fedbe9b454ed45cd411124d3241b8670d1e08
mhamza-ali <hamza4841333@gmail.com>
2023-09-20 23:47:00 +0500

    operarions done

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,40 @@
+let score = "33"
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+console.log(typeof(valueInNumber))
+console.log(valueInNumber)
+
+//"33" => 33
+// "33abc" => NaN
+// true => 1; false => 0;
+
+//**********Operations ***********
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+let str1 = 'hello'
+let str2= "JS"
+console.log(str1 + str2)
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
+
+let num1, num2, num3 
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter)
+
+// let a = 3;
+// let b = ++a; // pre increment
+// console.log(a, b) // 4 4
+
+let a = 3;
+let b = a++; // post increment
+console.log(a, b) // 4 3
+
