7c3059dbf39830066d4a128f42a7100eceb62b67
mhamza-ali <hamza4841333@gmail.com>
2023-09-20 22:41:11 +0500

    variables learned

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -4,8 +4,13 @@ console.log("Hello world in js")
 const accountId = 12 // change of value is not allowed
 let accountEmail = "hello@gmail.com"
 var accountPassword = "12345"
-accountCity = "Karachi"
-
+let accountState;
 console.log(accountId)
-console.table([accountId, accountEmail, accountPassword])
+console.table([accountId, accountEmail, accountPassword, accountState])
+
+/* 
+prefer ot to use var because of issue in block scope and
+*/
+
+
 
