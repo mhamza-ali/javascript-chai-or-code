efdf8e809aa0a5aca81b217c80a4a5b94ef2d998
mhamza-ali <hamza4841333@gmail.com>
2023-09-20 22:56:40 +0500

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3 + 3)
+
+let name = "JS" //string
+let age  = 18   //number
+let isLoggIn = false //boolean
+let state = null //null
+let school; // undefined
+//symbol for unique
+//object
+
+//console.log(typeof age)
+
+
